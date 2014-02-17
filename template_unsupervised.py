d42c4c163d6942850abc77cd1fd373e3d2ca9d74
Edward Hunter <edwardhunter@ucsd.edu>
2014-02-16 18:03:41 -0800

    Initial unsupervised template.

---
+++ template_unsupervised.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+@package css
+@file css/template_unsupervised.py
+@author Edward Hunter
+@author Your Name Here
+@brief A template to be customized for unsupervised learning experiments.
+"""
+
+# Import common modules and utilities.
+from common import *
