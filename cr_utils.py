689b022d0cb14bdab0ec22d57fc83e62b16e7e6b
Edward Hunter <edwardhunter@ucsd.edu>
2014-03-15 17:36:17 -0700

    Refined filter logic. Added dw nominate parsing.

--- cr_utils.py
+++ cr_utils.py
@@ -2,7 +2,7 @@
 
 """
 @package css
-@file css/cr_retrieve.py
+@file css/cr_utils.py
 @author Edward Hunter
 @brief Utility module for retrieving congress.gov data.
 """
@@ -22,59 +22,92 @@ from bs4 import BeautifulSoup
 #<a href="http://beta.congress.gov/congressional-record/2009/08/07/senate-section/article/S9065-2">prayer</a>
 #http://beta.congress.gov/congressional-record/2009/08/07/senate-section/article/S9078-8
 
+# DW Nominate Senate data URL.
+#ftp://voteview.com/junkord/SL01112D21_BSSE.dat
+
+# DW Nominate line format and regex pattern.
+# 112 29936 48 0 SOUTH ?? 200 DEMINT   ??  0.900  0.010  0.172  0.206  0.563   -59.22317  369   26  0.852
+# regex group      1       2       3       4       5           6       7       8              9             10            11            12            13            14            15      16      17
+DWN_PATTERN = r'\s*(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+([ A-Z]+)\s+(\d+)\s+([A-Z]+)([ A-Z\.]+)?\s+(-?[\d\.]+)\s+(-?[\d\.]+)\s+(-?[\d\.]+)\s+(-?[\d\.]+)\s+(-?[\d\.]+)\s+(-?[\d\.]+)\s+(\d+)\s+(\d+)\s+(-?[\d\.]+)'
+DWN_EXPR = re.compile(DWN_PATTERN)
+
+# DW Nominate fields.
+"""
+1 'congress',
+2 'icpsr',
+3 'state_code',
+4 'district',
+5 'state_name',
+6 'party',
+7 'name',
+8 'coord_1',
+9 'coord_2',
+10 'coord_1_error',
+11 'coord_2_error',
+12 'corr',
+13 'log_likelihood',
+14 'no_votes',
+15 'no_errors',
+16'geom_mean_prob',
+"""
+
+DW_NOMINATE_FNAME = 'dw_nominate.txt'
+
 # Possible network errors to be handled.
 # IOError: [Errno socket error] [Errno 60] Operation timed out
 # IOError: [Errno 54] Connection reset by peer
 
+"""
+Load combined files.
+import pickle
+x = pickle.loads(open('congress_112_combined.pkz','rb').read().decode('zip'))
+"""
 
 BASE_URL = 'http://beta.congress.gov'
-
-PARA_START_EXPRS = re.compile(r'^\s{2}\w+',re.MULTILINE)
-SPEECH_EXPR = re.compile(r'^ {,3}[^\s]+.*\n', re.MULTILINE)
-#SPEAKER_EXPR = re.compile(r' {,3}(Mr.|Ms.|Mrs.) ([A-Za-z]+)(of \w+)?\.')
-SPEAKER_EXPR = re.compile(r' {,3}(Mr.|Ms.|Mrs.) ([A-Za-z]+)\.?( of \w+\.)?')
+SPEAKER_EXPR = re.compile(r' {,3}(Mr.|Ms.|Mrs.) ([A-Z][A-Za-z]+[A-Z])( of [^\.]+)?\.')
 
 # These patterns signale the end or interruption in a speech.
-IGNORE_PATTERNS = [
+STOP_PATTERNS = [
+    # Observed in congress 112, 111, 110, 109
+    r'\s+The (ACTING |VICE )?PRESIDENT',
     r'\s+The PRESIDING OFFICER',
-    r'\s+ There being no objection',
-    r'\s+The ACTING PRESIDENT',
-    r'\s+The VICE PRESIDENT',
-    r'\s+The (assistant )?legislative clerk',
-    r'\s+The resolution|\s+The preamble',
+    r'\s+The (articles?|letters?) follows?',
+    r'\s+The (resolution|preamble)',
     r'\s+At the request of',
     r'\s+The following .*were',
     r'\s+The yeas and nays',
+    r'\s+(Mr.|Ms.|Mrs.) [A-Za-z]+ (thereupon|addressed the)',
+    r'\s+By (Mr.|Ms.|Mrs.) [A-Za-z]+.*',
     r'\s+S\.|\s+SA \d+',
-    r'\s+At the request of',
-    r'\s+The following .*were',
-    r'\s+The yeas and nays',
-    r'\s+[^\.]+submitted the following',
-    r'\s+The (bill )?clerk',
+    r'\s+The (bills?|results?|amendments?) (was|were)',
+    r'\s+There being no objection',
+    r'\s+The (assistant )?(legislative|bill) clerk',
     r'\s+Thereupon, the Senate',
+    r'\s+Exhibit [A-Za-z0-9]+',
+    # The following may be redundant or unnecessary.
+    r'\s+[^\.]+submitted the following',
     r'\s+Pending:',
-    r'\s+(Mr.|Ms.|Mrs.) [A-Za-z]+ thereupon',
     r'\s+The Senate met',
-    r'\s+The article follows',
-    r'\s+The letter follows',
     r'\s+A message from the .+ was',
     r'\s+At .+(a|p)\.m\., a message from the',
     r'\s+\w+ received by the Senate:',
     r'\s+\w+There being no objection',
-    r'\s+.+addressed the .+\.',                 #Mrs. HUTCHISON addressed the chair.
 ]
 
 # These patterns are non speech content to be removed.
-SUB_PATTERNS =[
-    r'\[+.+]+\n+',
-    r'__+',
-    r'^\n'
+REMOVE_PATTERNS =[
+    #r' \n\n *\[.+\]\n\n',
+    r' *\[.+\]',
+    r' *__+',
+    r' {10,}[^ ].*'
 ]
 
+# Network error retries and sleep time.
 NO_TRIES = 10
+SLEEP_TIME = 10
 
-IGNORE_EXPRS = [re.compile(x) for x in IGNORE_PATTERNS]
-SUB_EXPRS = [re.compile(x) for x in SUB_PATTERNS]
+STOP_EXPRS = [re.compile(x) for x in STOP_PATTERNS]
+REMOVE_EXPRS = [re.compile(x) for x in REMOVE_PATTERNS]
 
 
 def download_articles_by_congress(congress_no, limit=None):
@@ -121,7 +154,7 @@ def download_articles_by_congress(congress_no, limit=None):
             tries += 1
             if tries > NO_TRIES:
                 raise
-            time.sleep(10)
+            time.sleep(SLEEP_TIME)
 
     # For each senate tag in the congress.
     starttime = time.time()
@@ -149,7 +182,7 @@ def download_articles_by_congress(congress_no, limit=None):
                 tries += 1
                 if tries > NO_TRIES:
                     raise
-                time.sleep(10)
+                time.sleep(SLEEP_TIME)
 
         # For each article tag in the daily transcript.
         for tag2 in tags2:
@@ -180,7 +213,7 @@ def download_articles_by_congress(congress_no, limit=None):
                     tries += 1
                     if tries > NO_TRIES:
                         raise
-                    time.sleep(10)
+                    time.sleep(SLEEP_TIME)
 
             # Extract article text.
             if len(tags3)!=1:
@@ -203,51 +236,52 @@ def filter_lines(art, verbose=False):
     Remove non speech lines and text.
     @param art: Article to filter.
     @param verbose: Boolean to output filtered text during run for debugging.
-    @result: Article filtered of non speech lines and text.
+    @return (result, patterns): Tuple of article text filtered for speech
+    text only, set of unique regex patterns used in detecting speech endings.
     """
     result = ''
+    patterns = set()
     current_speaker = None
     if verbose:
-        print '-'*100
-        print 'NO SPEAKER:'
+        print '-'*80
+        print 'BEGIN:'
 
-    for x in SPEECH_EXPR.finditer(art):
-
-        line = x.group(0)
+    lines = art.split('\n')
+    for i, line in enumerate(lines):
         speaker_match = SPEAKER_EXPR.match(line)
-
-        # Case 1: line identifies a new speaker. Keep line.
         if speaker_match:
             current_speaker = speaker_match.group(2)
-            #line = SPEAKER_EXPR.sub('',line)
             if verbose:
-                print '-'*100
+                print '-'*80
                 print 'SPEAKER: ' + repr(current_speaker)
+                print line
 
-        # Case 2: existing speaker.
-        # If interruption, reset speaker and eliminate line.
         elif current_speaker:
-            for y in IGNORE_EXPRS:
-                if y.match(line):
+            for y in STOP_EXPRS:
+                ymatch = y.match(line)
+                if ymatch:
+                    patterns.update([ymatch.re.pattern])
                     current_speaker = None
                     if verbose:
-                        print '-'*100
+                        print '-'*80
                         print 'NO SPEAKER:'
                     break
 
-        # Case 3: No existing speaker, line invalid.
         else:
             pass
 
         # If line valid, remove non speech text and append line.
         if current_speaker:
-            for z in SUB_EXPRS:
+            for z in REMOVE_EXPRS:
                 line = z.sub('', line)
             result += line
+            if len(line)>0:
+                result += '\n'
+
         if verbose:
             print line
 
-    return result
+    return result, patterns
 
 
 def segment_speakers(art):
@@ -274,18 +308,103 @@ def segment_speakers(art):
             speech += '\n'
             speech += line
 
+    if len(speech) >0 and current_speaker:
+        result.append((current_speaker, speech))
+
     return result
 
 
-def make_fname(congress_no, type):
+def combine_speeches(speeches):
+    """
+    Combine speeches from each speaker into a single document keyed
+    by speaker name.
+    @param sppeches. List of speech tuples.
+    @return result: dict of combined speeches.
+    """
+    result = {}
+    for speech in speeches:
+        speaker = speech[4]
+        if speaker not in result.keys():
+            result[speaker] = ''
+        result[speaker] += speech[5]
+        result[speaker] += '\n\n'
+    return result
+
+
+def make_fname(congress_no, type, ext='pkz'):
     """
     Create filename for CR download or speech files.
     @param congress_no: int congress number.
     @param type: the file type string.
     @return fname: the file name.
     """
-    fname = 'congress_%i_%s.pk' % (congress_no, type)
+    fname = 'congress_%i_%s.%s' % (congress_no, type, ext)
     return fname
+
+
+def create_dwn(fname=DW_NOMINATE_FNAME):
+    """
+    Parse and save a dw-nominate data object
+    @param fname: File name for dw-nominate senate data.
+    @return result: xxx.
+    """
+
+    # Read in dw nominate file.
+    try:
+        f = open(fname)
+    except:
+        # download and open dw nominate data.
+        pass
+
+    data = f.read()
+    f.close()
+
+    # Parse lines into a dict.
+    scores_dict = {}
+    lines = data.split('\n')
+    for line in lines:
+        m = DWN_EXPR.match(line)
+        if m:
+            congress            = int(m.group(1))
+            icpsr               = int(m.group(2))
+            state_code          = int(m.group(3))
+            district            = int(m.group(4))
+            state               = m.group(5).lstrip().rstrip()
+            party               = int(m.group(6))
+            name                = m.group(7).lstrip().rstrip()
+            coord_1             = float(m.group(9))
+            coord_2             = float(m.group(10))
+            coord_1_error       = float(m.group(11))
+            coord_2_error       = float(m.group(12))
+            corr                = float(m.group(13))
+            log_likelihood      = float(m.group(14))
+            no_votes            = int(m.group(15))
+            no_errors           = int(m.group(16))
+            geom_mean_prob      = float(m.group(17))
+            key = (congress, name)
+            val = [congress, icpsr, state_code, district, state, party,
+                   name, coord_1, coord_2, coord_1_error, coord_2_error,
+                   corr, log_likelihood, no_votes, no_errors, geom_mean_prob]
+            scores_dict[key] = val
+
+    f = open('dw_nominate.pkz','wb')
+    f.write(pickle.dumps(scores_dict).encode('zip'))
+    f.close()
+
+
+def create_labled_data(congress_no_start, congress_no_stop,
+                       fname=DW_NOMINATE_FNAME):
+    """
+    Create labled data for a given contress.
+    @param congress_no: Int congress number to label.
+    """
+    try:
+        f = open('dw_nominate.pkz','rb')
+    except:
+        create_dwn(fname)
+        f = open('dw_nominate.pkz','rb')
+    scores = pickle.loads(f.read().decode('zip'))
+    f.close()
 
 
 if __name__ == '__main__':
@@ -301,8 +420,12 @@ if __name__ == '__main__':
                  type='int', help='Limit the number of article downloads.')
     p.add_option('-p','--process', action='store_true', dest='process',
                 help='Process congress file for speech content.')
-    p.add_option('-s','--show', action='store_true', dest='show',
-                help='Dump speech data to console.')
+    p.add_option('-t','--text', action='store_true', dest='text',
+                help='Create text file of processed congress data.')
+    p.add_option('-v','--verbose', action='store_true', dest='verbose',
+                help='Dump debugging data to console.')
+    p.add_option('-e','--expressions', action='store_true', dest='expressions',
+                help='Dump stop expression data to console.')
 
     (opts, args) = p.parse_args()
     if len(args) < 1:
@@ -315,35 +438,57 @@ if __name__ == '__main__':
         limit = opts.limit
         articles = download_articles_by_congress(congress_no, limit)
         fname = make_fname(congress_no, 'download')
-        f = open(fname, 'w')
-        pickle.dump(articles, f)
+        f = open(fname, 'wb')
+        f.write(pickle.dumps(articles).encode('zip'))
         f.close()
 
     processed_speeches = []
+    all_patterns = set()
     if opts.process:
         if not articles:
             fname = make_fname(congress_no, 'download')
-            f = open(fname)
-            articles = pickle.load(f)
+            f = open(fname, 'rb')
+            articles = pickle.loads(f.read().decode('zip'))
             f.close()
+
         for i in range(len(articles)):
-            filtered_text = filter_lines(articles[i][4])
+            filtered_text, patterns = filter_lines(articles[i][4], opts.verbose)
+            all_patterns.update(patterns)
             speeches = segment_speakers(filtered_text)
             for speech in speeches:
                 processed_speeches.append((articles[i][0], articles[i][1],
                         articles[i][2], articles[i][3], speech[0], speech[1]))
+        speech_dict = combine_speeches(processed_speeches)
+
         fname = make_fname(congress_no, 'speeches')
-        f = open(fname, 'w')
-        pickle.dump(processed_speeches, f)
+        f = open(fname, 'wb')
+        f.write(pickle.dumps(processed_speeches).encode('zip'))
+        f.close()
+        fname = make_fname(congress_no, 'combined')
+        f = open(fname, 'wb')
+        f.write(pickle.dumps(speech_dict).encode('zip'))
         f.close()
 
-    if opts.show:
+        if opts.verbose or opts.expressions:
+            print '-'*80
+            print 'STOP PATTERNS:'
+            for x in all_patterns:
+                print repr(x)
+
+    if opts.text:
         if not processed_speeches:
             fname = make_fname(congress_no, 'speeches')
-            processed_speeches = pickle.load(open(fname))
+            f = open(fname, 'rb')
+            articles = pickle.loads(f.read().decode('zip'))
+            f.close()
+        fname = make_fname(congress_no,'dump','txt')
+        f = open(fname,'w')
         for x in processed_speeches:
-            print '-'*80
-            print '%i %s %s %s\n%s' % (x[0], str(x[1]), x[2], x[4], x[3])
-            print '-'*80
-            print x[5]
+            metadata = '\n\n' + '-'*120 + '\n'
+            metadata += 'congress: %i date: %s article: %s speaker: %s' % \
+                        (x[0], str(x[1]), x[2], x[4])
+            metadata += '\ntitle: %s\n\n' % x[3]
+            f.write(metadata)
+            f.write(x[5])
+        f.close()
 
