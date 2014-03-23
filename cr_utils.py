342e93460ed2bc392bec18bc376aa09de4341720
Edward Hunter <edwardhunter@ucsd.edu>
2014-03-23 11:06:51 -0700

    Remove dw_nominate.txt, add copyright and GPL license.

--- cr_utils.py
+++ cr_utils.py
@@ -7,6 +7,27 @@
 @brief Utility module for retrieving congress.gov data.
 """
 
+# Copyright and licence.
+"""
+Copyright (C) 2014 Edward Hunter
+edward.a.hunter@gmail.com
+840 24th Street
+San Diego, CA 92102
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from common import *
 
 # TODO: Add timeouts for hanging URL reads.
