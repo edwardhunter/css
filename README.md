aec6b704f2aefb558d96050607fd20b6eab851f8
Edward Hunter <edwardhunter@ucsd.edu>
2014-03-28 09:44:20 -0700

    Remove spkm notes, add readme.

--- README.md
+++ README.md
@@ -1,4 +1,81 @@
-css
-===
+CSS Package for Python Computational Social Science
+===================================================
 
-Laboratory code for CSS class Soc 290 at UCSD.
+Introduction
+------------
+
+This is the readme file for the python css package,
+developed by Edward Hunter for testing and evaluating machine
+learning algorithms for Computational Social Science application
+in research and coursework. It was first used as courseware
+in UCSD's Soc-211 "Introduction to Computational
+Methods in Social Science," in Spring 2014. Algorithms are based
+on scikit-learn and original implementations. See below for
+instructions on installing dependencies on various operating systems.
+
+Copyright
+---------
+
+Copyright (C) 2014 Edward Hunter
+edward.a.hunter@gmail.com
+840 24th Street
+San Diego, CA 92102
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Installing Dependencies and Setup
+=================================
+
+The following software is required or advised for use of this package:
+
+1. Python: Version 2.7.6 is used and tested.
+2. A decent source code editor. Edward recommends PyCharm.
+3. git: Distributed source code control for access to this repository.
+4. easy_install/setuptools: Python package installer. Part of the
+setuptools 3rd party module that extends Python's native distutils module.
+5. pip: Better package installer improving on easy_install.
+6. virtualenv and virtualenvwrapper: Support for Python virtual environments.
+7. numpy: Numerical Python library. Has additional dependencies.
+8. scipy: Scientific Python library extending numpy. Has additional
+dependencies.
+9. matplotlib: Graphics and visualization Python library. Has additional
+dependencies.
+10. nltk: Python natural language processing library.
+11. BeautifulSoup4: High level Python HTML parsing library.
+
+Current Tested Versions
+-----------------------
+
+TODO
+
+Installation on Mac OS X and Linux
+----------------------------------
+
+TODO
+
+Installation on Windows
+-----------------------
+
+TODO
+
+Making a Github Account and Cloning the CSS Repository
+------------------------------------------------------
+
+TODO
+
+Testing the Installation
+------------------------
+
+TODO
+
--- spkm.py
+++ spkm.py
@@ -99,18 +99,15 @@ def spkmeans(x, no_clusters, verbose=False, **kwargs):
         # Fix empty clusters here.
         empty = [i for i in cluster_ids if i not in r_new]
         if empty:
-            if verbose:
-                print 'Iteration %i: empty clusters: %s' % (count, str(empty))
+            print 'Iteration %i: empty clusters: %s' % (count, str(empty))
             if on_empty == 'restart':
-                if verbose:
-                    print 'Reinitializing algorithm.'
+                print 'Reinitializing algorithm.'
                 shuffled_docs, r_new, mu = initialize_clusters(x, cluster_ids,
                                                                doc_ids)
                 continue
 
             else:
-                if verbose:
-                    print 'Reassinging remote data to empty clusters.'
+                print 'Reassinging remote data to empty clusters.'
                 for i,j in enumerate(empty):
                     r_new[scores_idx[-(i+1)]] = j
                 empty = [i for i in cluster_ids if i not in r_new]
@@ -134,7 +131,7 @@ def spkmeans(x, no_clusters, verbose=False, **kwargs):
         for j in range(no_clusters):
             mu_norm = np.linalg.norm(mu[j,:])
             if mu_norm == 0.0:
-                print 'Cluster %i still EMPTY!' % j
+                print 'WARNING: Cluster %i empty!' % j
 
             else:
                 mu[j,:] = mu[j,:]/mu_norm
@@ -144,221 +141,4 @@ def spkmeans(x, no_clusters, verbose=False, **kwargs):
             print 'Iteration %i: %.2f seconds.' % (count, delta)
         count += 1
 
-"""
-no_docs = x_train.shape[0]
-dim = x_train.shape[1]
-print 'no docs: %i' % no_docs
-print 'dimensionality: %i' % dim
 
-similarities = []
-cluster_sizes = []
-mus = []
-counts = []
-r = []
-
-for i in range(10):
-    sim_i, count_i, r_i, mu_i = spkmeans(x_train, no_clusters, 'reassign')
-
-    # Count the number in each cluster.
-    cluster_sizes_i = count_cluster_sizes(no_clusters, no_docs, r_i)
-
-    print 'Run %i: %i iterations, %f similarity, sizes: %s' %\
-          (i, count_i, sim_i[-1], str(cluster_sizes_i))
-
-    cluster_sizes.append(cluster_sizes_i)
-    similarities.append(sim_i)
-    mus.append(mu_i)
-    counts.append(count_i)
-    r.append(r_i)
-"""
-
-
-"""
-# Plotting score functions.
-for y in similarities:
-    x = range(len(y))
-    plt.plot(x,y,'k-')
-    b = [0,y[-1]]
-    a = [len(y)-1, len(y)-1]
-    plt.plot(a,b,'r:')
-    plt.xlabel('Total Similarity')
-    plt.ylabel('Sorted Run')
-plt.savefig('similarity_curves.png')
-"""
-
-"""
-# Plotting sorted best scores for the ensemble.
-best_sims = [x[-1] for x in similarities]
-best_sims.sort(reverse=True)
-x = range(len(best_sims))
-plt.plot(x, best_sims, 'r-')
-plt.xlabel('Total Similarity')
-plt.ylabel('Sorted Run')
-plt.show()
-"""
-
-"""
-# You can specify a rotation for the tick labels in degrees or with keywords.
-plt.xticks(x, labels, rotation='vertical')
-# Pad margins so that markers don't get clipped by the axes
-plt.margins(0.2)
-# Tweak spacing to prevent clipping of tick-labels
-plt.subplots_adjust(bottom=0.15)
-plt.show()
-"""
-
-
-# Plotting cluster vectors.
-"""
-#plt.subplot(2,2,1)
-plt.cla()
-sort_idx = np.argsort(mus[0][0,:])[-10:]
-sort_mu = mus[0][0,sort_idx]
-x_ticks = []
-x_labels = []
-for i in range(len(sort_mu)):
-    x = [sort_idx[i], sort_idx[i]]
-    y = [0, 1.0]
-    plt.plot(x,y,'r-')
-y = mus[0][0,:]
-x = range(len(y))
-plt.plot(x,y,'k-')
-plt.axis([0, len(y), 0, 1])
-plt.xlabel('Term Feature')
-plt.ylabel('TFIDF Score')
-for i,idx in enumerate(sort_idx[::-1]):
-    name = feature_names[feature_idx[idx]]
-    plt.text(idx, 0.95-(i*0.035) ,name)
-figpath = 'cluster1.png'
-plt.savefig(figpath)
-
-#plt.subplot(2,2,2)
-plt.cla()
-sort_idx = np.argsort(mus[0][1,:])[-10:]
-sort_mu = mus[0][1,sort_idx]
-for i in range(len(sort_mu)):
-    x = [sort_idx[i], sort_idx[i]]
-    y = [0, 1.0]
-    plt.plot(x,y,'r-')
-y = mus[0][1,:]
-x = range(len(y))
-plt.plot(x,y,'k-')
-plt.axis([0, len(y), 0, 1])
-plt.xlabel('Term Feature')
-plt.ylabel('TFIDF Score')
-for i,idx in enumerate(sort_idx[::-1]):
-    name = feature_names[feature_idx[idx]]
-    plt.text(idx, 0.95-(i*0.035) ,name)
-figpath = 'cluster2.png'
-plt.savefig(figpath)
-
-#plt.subplot(2,2,3)
-plt.cla()
-sort_idx = np.argsort(mus[0][2,:])[-10:]
-sort_mu = mus[0][2,sort_idx]
-for i in range(len(sort_mu)):
-    x = [sort_idx[i], sort_idx[i]]
-    y = [0, 1.0]
-    plt.plot(x,y,'r-')
-y = mus[0][2,:]
-x = range(len(y))
-plt.plot(x,y,'k-')
-plt.axis([0, len(y), 0, 1])
-plt.xlabel('Term Feature')
-plt.ylabel('TFIDF Score')
-for i,idx in enumerate(sort_idx[::-1]):
-    name = feature_names[feature_idx[idx]]
-    plt.text(idx, 0.95-(i*0.035) ,name)
-figpath = 'cluster3.png'
-plt.savefig(figpath)
-
-#plt.subplot(2,2,4)
-plt.cla()
-sort_idx = np.argsort(mus[0][3,:])[-10:]
-sort_mu = mus[0][3,sort_idx]
-for i in range(len(sort_mu)):
-    x = [sort_idx[i], sort_idx[i]]
-    y = [0, 1.0]
-    plt.plot(x,y,'r-')
-y = mus[0][3,:]
-x = range(len(y))
-plt.plot(x,y,'k-')
-plt.axis([0, len(y), 0, 1])
-plt.xlabel('Term Feature')
-plt.ylabel('TFIDF Score')
-for i,idx in enumerate(sort_idx[::-1]):
-    name = feature_names[feature_idx[idx]]
-    plt.text(idx, 0.95-(i*0.035) ,name)
-figpath = 'cluster4.png'
-plt.savefig(figpath)
-
-#plt.show()
-"""
-
-
-
-
-"""
-NOTES
-
-feature_names = vectorizer.get_feature_names()
-sort_idx = np.argsort(mu)
-print 'Features: ' + str(len(feature_names))
-print 'Mu shape: ' + str(mu.shape)
-print 'sort_idx shape: ' + str(sort_idx.shape)
-print 'Similarity: ' + str(similarity)
-
-for j in range(no_clusters):
-    print '-'*80
-    print 'Top features for cluster %i' %j
-    for i in range(20):
-        print feature_names[sort_idx[j,i]]
-
-
-        if confusion_image_type == 'log':
-            log_conf_matrix = np.log10(conf_matrix+1)
-            plt.pcolor(np.flipud(log_conf_matrix))
-            title = '%s %s Log Confusion, %s' % (METHOD, model, dataset)
-        elif confusion_image_type == 'linear':
-            plt.pcolor(np.flipud(conf_matrix))
-            title = '%s %s Confusion, %s' % (METHOD, model, dataset)
-        plt.xticks(np.arange(n)+0.5, np.arange(1,n+1))
-        plt.yticks(np.arange(n)+0.5, np.arange(n,0, -1))
-        plt.xlabel('Predicted Category')
-        plt.ylabel('True Category')
-        plt.set_cmap('hot')
-        plt.colorbar()
-        plt.title(title)
-        figpath = os.path.join(REPORT_HOME, figfname)
-        plt.savefig(figpath)
-
-for i in range(no_clusters):
-    print np.linalg.norm(mus[0][i,:])
-
-
-"""
-
-"""
-# Import common modules and utilities.
-from common import *
-
-#
-no_clusters = 4
-dim = 200
-
-# Load training/testing utilities.
-from data_utils import load_data, DATASETS
-
-# Load and extract data.
-data = load_data('20news4')
-data_train = data['train']
-data_train_target = data['train_target']
-
-
-# Vectorize data.
-# (10729, 99299) - 10K docs, 100K features
-vectorizer = TfidfVectorizer(stop_words='english',sublinear_tf=True,
-                min_df=10, max_df=0.9)
-x_train = vectorizer.fit_transform(data_train)
-feature_names = vectorizer.get_feature_names()
-"""
